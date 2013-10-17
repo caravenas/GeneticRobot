b8b05f2b58d0005e956985014759b11775d50fd1
Christopher Aravena <christopher.aravena@gmail.com>
2013-10-17 17:43:33 -0300

    - Added flow screens

---
+++ Assets/Scripts/FlowScreen.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FlowScreen : MonoBehaviour {
+	
+	
+	
+	public enum MenuScreen
+	{
+		Intro,
+		Main,
+		Tutorial,
+		MakerRobot,
+		MyRobots,
+	}
+	
+	public enum Trigger
+	{
+		OnClick,
+		OnMouseOver,
+		OnMouseOut,
+		OnPress,
+		OnRelease,
+	}
+	
+	public Trigger trigger = Trigger.OnClick;
+	public MenuScreen m_screen = MenuScreen.Intro;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnClick ()
+	{
+		if (enabled && trigger == Trigger.OnClick)
+		{
+			switch(m_screen){
+				
+				case MenuScreen.Intro:
+					Application.LoadLevel("Intro");
+					break;
+				case MenuScreen.Main:
+					Application.LoadLevel("Main");
+					break;
+				case MenuScreen.Tutorial:
+					Application.LoadLevel("Tutorial");
+					break;
+				case MenuScreen.MakerRobot:
+					Application.LoadLevel("MakerRobot");
+					break;
+				case MenuScreen.MyRobots:
+					Application.LoadLevel("MyRobots");
+					break;
+			}
+			
+
+		}
+	}
+}
